645fc34fc455643be34b869b98461a41c861845d
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-04-20 17:54:26 -0500

    1 test passing :)

--- endofday/tests/test_executors.py
+++ endofday/tests/test_executors.py
@@ -4,19 +4,31 @@ Tests for the executors module.
 
 import pytest
 
-import agavepy
+from endofday.core.executors import AgaveExecutor
 
+SYSTEM_ID = 'data.iplantcollaborative.org'
 
 @pytest.fixture(scope='session')
-def agave(credentials):
-    ag = agavepy.agave.Agave(resources=credentials['resources'],
-                  username=credentials['username'],
-                  password=credentials['password'],
-                  api_server=credentials['apiserver'],
-                  api_key=credentials['apikey'],
-                  api_secret=credentials['apisecret'],
-                  verify=credentials.get('verify_certs', True))
-    ag.token.create()
-    return ag
+def ae():
+    ae = AgaveExecutor(wf_name='test_suite_wf')
+    return ae
+
+
+def test_create_dir(ae):
+    # first, delete the test dir just in case it is already there:
+    try:
+        ae.ag.files.deleteFromDefaultSystem(sourcefilePath='jstubbs/test_suite_wf')
+    except:
+        pass
+    # create the directory:
+    ae.create_dir('test_suite_wf/algebra/add_5/data')
+    # check that it exists:
+    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf')
+    assert len(rsp) > 0
+    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algrebra')
+    assert len(rsp) > 0
+    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algrebra/add_5')
+    assert len(rsp) > 0
+
 
 
