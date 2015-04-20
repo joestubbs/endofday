8351470024708b89939c2b92287ace2d6452efa9
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-04-20 16:02:51 -0500

    Fixing Dockerfile for relative imports.

--- endofday/core/endofday.sh
+++ endofday/core/endofday.sh
@@ -3,11 +3,11 @@
 ARG=$1
 
 if [ $ARG = "--setup" ]; then
-  cp /endofday/alias.sh /staging/endofday.sh
+  cp /core/alias.sh /staging/endofday.sh
   chmod +x /staging/endofday.sh
 else
-  cd /endofday
-  python /endofday/tasks.py $STAGING/$ARG
+  cd /
+  python -m core.tasks $STAGING/$ARG
   # This used only for the nf backend
 #  /nextflow run "${ARG%.*}.nf"
 fi
