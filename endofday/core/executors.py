3b8aff0a1520d19e03300056e6c26acb82bdcb1a
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-04-22 13:08:50 -0500

    Tests now passing intermitently, though there is an issue with the Agave history service failing initially.

--- endofday/core/executors.py
+++ endofday/core/executors.py
@@ -2,10 +2,79 @@
 # Implements support for remote execution platforms such as agave.
 
 import os
+import requests
+import time
+
 from agavepy.agave import Agave
 
 from .error import Error
 from .config import Config
+
+class TimeoutError(Error):
+    pass
+
+class AgaveAsyncResponse(object):
+    """
+    Implements parts of the concurrent.futures.Future interface for nonblocking Agave responses.
+    """
+    def __init__(self, ag, response):
+        """Construct an asynchronous response object from an Agave response which should be an agavepy.agave.AttrDict
+        """
+        self.ag = ag
+        self.status = response.status
+        self.url = response._links.get('history').get('href')
+        if not self.url:
+            raise Error("Error parsing response object: no URL detected. response: " + str(response))
+        # url's returned by agave sometimes have the version as 2.0, so we replace that here
+        self.url = self.url.replace('/2.0/','/v2/')
+
+    def _update_status(self):
+        headers = {'Authorization': 'Bearer ' + self.ag.token.token_info.get('access_token')}
+        rsp = requests.get(url=self.url, headers=headers)
+        if not rsp.status_code == 200:
+            import pdb; pdb.set_trace()
+            raise Error("Error updating status; invalid status code:  " + str(rsp.status_code) + str(rsp.content))
+        result = rsp.json().get('result')
+        if not result:
+            raise Error("Error updating status: " + str(rsp))
+        # if the transfer ever completed, we'll call it good:
+        if len([x for x in result if 'COMPLETE' in x['status']]) > 0:
+            self.status = 'COMPLETE'
+        else:
+            # sort on creation time of the history object
+            result = sorted(result, key=lambda k: k['created'])
+            self.status = result[0].get('status')
+
+    def _is_done(self):
+        return self.status == 'COMPLETE' or self.status == 'STAGING_FAILED'
+
+    def done(self):
+        """Return True if the call was successfully cancelled or finished running."""
+        self._update_status()
+        return self._is_done()
+
+    def result(self, timeout=None):
+        """
+        Returns the result of the original call, blocking until the result is returned or the timeout parameter is
+        reached. The timeout paramter is interpreted in seconds.
+        :param timeout:
+        :return:
+        """
+        if self._is_done():
+            return self.status
+        self._update_status()
+        now = time.time()
+        if timeout:
+            future = now + timeout
+        else:
+            future = float("inf")
+        while time.time() < future:
+            time.sleep(1)
+            self._update_status()
+            if self._is_done():
+                return self.status
+        raise TimeoutError()
+
 
 class AgaveExecutor(object):
     """Execute a task in the Agave cloud"""
@@ -34,13 +103,16 @@ class AgaveExecutor(object):
                         client_name=client_name, api_key=client_key, api_secret=client_secret)
         self.storage_system = storage_system
         self.wf_name = wf_name
-        if home_dir == '/':
-            raise Error('Invalid home_dir: /, path must be relative to ')
         self.home_dir = home_dir
         # default the home directory to the username
         if not self.home_dir:
             self.home_dir = username
         self.working_dir = os.path.join(self.home_dir, wf_name)
+        # create the working directory now
+        rsp = self.ag.files.manage(systemId=self.storage_system,
+                                   filePath=self.home_dir,
+                                   body={'action':'mkdir',
+                                         'path':wf_name})
 
     def from_config(self):
         """
@@ -75,25 +147,27 @@ class AgaveExecutor(object):
         """
         if path.startswith('/'):
             path = path[1:]
-        path = os.path.join(self.working_dir, path)
         try:
             rsp = self.ag.files.manage(systemId=self.storage_system,
-                                       filePath=self.home_dir,
+                                       filePath=self.working_dir,
                                        body={'action':'mkdir',
                                              'path':path})
         except Exception as e:
             raise Error("Error creating directory on default storage system. Path: " + path +
                         "Msg:" + str(e))
+        return rsp
 
     def upload_file(self, local_path, remote_path):
         """Upload a file on the local system to remote storage. The remote_path param should
         be relative to the endofday working dir. The local_path should be absolute.
         """
-        sourcefilePath = os.join(self.working_dir, remote_path)
+        sourcefilePath = os.path.join(self.working_dir, remote_path)
         try:
-            self.ag.importData(systemId=self.storage_system,
-                               filePath=sourcefilePath,
-                               fileToUpload=open(local_path,'rb'))
+            rsp = self.ag.files.importData(systemId=self.storage_system,
+                                           filePath=sourcefilePath,
+                                           fileToUpload=open(local_path,'rb'))
         except Exception as e:
             raise Error("Upload to default storage failed - local_path: " + local_path +
                         "; remote_path: " + remote_path + "; Msg:" + str(e))
+        import pdb; pdb.set_trace()
+        return AgaveAsyncResponse(self.ag, rsp)
--- endofday/tests/test_executors.py
+++ endofday/tests/test_executors.py
@@ -2,11 +2,13 @@
 Tests for the executors module.
 """
 
+import os
 import pytest
-
 from endofday.core.executors import AgaveExecutor
 
 SYSTEM_ID = 'data.iplantcollaborative.org'
+
+HERE = os.path.dirname(os.path.abspath((__file__)))
 
 @pytest.fixture(scope='session')
 def ae():
@@ -17,18 +19,41 @@ def ae():
 def test_create_dir(ae):
     # first, delete the test dir just in case it is already there:
     try:
-        ae.ag.files.deleteFromDefaultSystem(sourcefilePath='jstubbs/test_suite_wf')
+        ae.ag.files.deleteFromDefaultSystem(sourcefilePath='jstubbs/test_suite_wf/algebra')
     except:
         pass
     # create the directory:
-    ae.create_dir('test_suite_wf/algebra/add_5/data')
+    ae.create_dir('algebra/add_5/data')
     # check that it exists:
     rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf')
-    assert len(rsp) > 0
-    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algrebra')
-    assert len(rsp) > 0
-    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algrebra/add_5')
-    assert len(rsp) > 0
+    assert type(rsp) == list
+    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algebra')
+    assert type(rsp) == list
+    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algebra/add_5')
+    assert type(rsp) == list
+
+def test_upload_file(ae):
+    # first, delete the test file just in case it is already there:
+    try:
+        ae.ag.files.deleteFromDefaultSystem(sourcefilePath='jstubbs/test_suite_wf/algebra/add_5/sample_input.txt')
+    except:
+        pass
+    # create the test directory
+    try:
+        ae.create_dir('algebra/add_5/data')
+    except:
+        pass
+    # upload the file
+    rsp = ae.upload_file(local_path=os.path.join(HERE,'sample_input.txt'), remote_path='algebra/add_5/data')
+    # returns an AgaveAsyncResponse - timeout after 120 seconds
+    assert rsp.result(120) == 'COMPLETE'
+    # check that the file is there:
+    rsp = ae.ag.files.listOnDefaultSystem(filePath='jstubbs/test_suite_wf/algebra/add_5/data')
+    assert type(rsp) == list
+    result = [r for r in rsp if r['name'] == 'sample_input.txt']
+    assert len(result) > 0
+
+
 
 
 
