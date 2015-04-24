32a37baace64109e4100b43e16222351ca1a201c
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-04-24 11:31:38 -0500

    mult_3 tests.

--- endofday/tests/test_tasks.py
+++ endofday/tests/test_tasks.py
@@ -35,6 +35,8 @@ def test_tasks_names_and_order(task_file):
     assert task_file.tasks[1].name == 'mult_3'
     assert task_file.tasks[2].name == 'sum'
 
+
+# add_5 tests
 def test_add_5_task_basic(task_file):
     task = task_file.tasks[0]
     assert task.name == 'add_5'
@@ -87,3 +89,82 @@ def test_add_5_docker_command(task_file):
     cmd, _, _ = task.get_docker_command()
     assert cmd == 'docker run --rm -v /staging/test_wf/add_5/data:/data -v /home/jstubbs/github-repos/endofday/examples/input.txt:/data/input.txt jstubbs/add_n python add_n.py -i 5'
 
+
+# mult_3 tests
+def test_mult_3_task_basic(task_file):
+    task = task_file.tasks[1]
+    assert task.name == 'mult_3'
+    assert task.image == 'jstubbs/mult_n'
+    assert task.command == 'python mult_n.py -f 3'
+    assert task.description == 'Multiply all inputs by 3.'
+    assert task.multiple == None
+    assert task.execution == 'local'
+
+def test_mult_3_inputs(task_file):
+    task = task_file.tasks[1]
+    assert len(task.inputs) == 1
+    inp = task.inputs[0]
+    assert inp.src == 'add_5.output'
+    assert inp.dest == '/tmp/input'
+    assert inp.src_name == 'output'
+    assert inp.src_task == 'add_5'
+
+def test_mult_3_outputs(task_file):
+    task = task_file.tasks[1]
+    assert len(task.outputs) == 4
+    out = task.outputs[0]
+    assert out.src == '/tmp/foo/bar/output'
+    assert out.label == 'output1'
+    assert out.task_name == 'mult_3'
+    assert out.eod_rel_path == 'test_wf/mult_3/tmp/foo/bar/output'
+    assert out.eod_rel_dir_path == 'test_wf/mult_3/tmp/foo/bar'
+    assert out.host_path == '/staging/test_wf/mult_3/tmp/foo/bar/output'
+    assert out.host_dir_path == '/staging/test_wf/mult_3/tmp/foo/bar'
+
+    out = task.outputs[1]
+    assert out.src == '/tmp/foo/baz/output'
+    assert out.label == 'output2'
+    assert out.task_name == 'mult_3'
+    assert out.eod_rel_path == 'test_wf/mult_3/tmp/foo/baz/output'
+    assert out.eod_rel_dir_path == 'test_wf/mult_3/tmp/foo/baz'
+    assert out.host_path == '/staging/test_wf/mult_3/tmp/foo/baz/output'
+    assert out.host_dir_path == '/staging/test_wf/mult_3/tmp/foo/baz'
+
+    out = task.outputs[2]
+    assert out.src == '/tmp/foo/output'
+    assert out.label == 'output3'
+    assert out.task_name == 'mult_3'
+    assert out.eod_rel_path == 'test_wf/mult_3/tmp/foo/output'
+    assert out.eod_rel_dir_path == 'test_wf/mult_3/tmp/foo'
+    assert out.host_path == '/staging/test_wf/mult_3/tmp/foo/output'
+    assert out.host_dir_path == '/staging/test_wf/mult_3/tmp/foo'
+
+    out = task.outputs[3]
+    assert out.src == '/tmp/output'
+    assert out.label == 'output'
+    assert out.task_name == 'mult_3'
+    assert out.eod_rel_path == 'test_wf/mult_3/tmp/output'
+    assert out.eod_rel_dir_path == 'test_wf/mult_3/tmp'
+    assert out.host_path == '/staging/test_wf/mult_3/tmp/output'
+    assert out.host_dir_path == '/staging/test_wf/mult_3/tmp'
+
+def test_mult_3_volume_dirs(task_file):
+    task = task_file.tasks[1]
+    assert len(task.volume_dirs) == 1
+    vdir = task.volume_dirs[0]
+    assert vdir.container_path == '/tmp'
+    assert vdir.eod_rel_path == 'test_wf/mult_3/tmp'
+    assert vdir.host_path == '/staging/test_wf/mult_3/tmp'
+
+def test_mult_3_input_volumes(task_file):
+    task = task_file.tasks[1]
+    assert len(task.input_volumes) == 1
+    inpv = task.input_volumes[0]
+    assert inpv.container_path == '/tmp/input'
+    assert inpv.eod_rel_path == 'test_wf/add_5/data/output.txt'
+    assert inpv.host_path == '/staging/test_wf/add_5/data/output.txt'
+
+def test_mult_3_docker_command(task_file):
+    task = task_file.tasks[1]
+    cmd, _, _ = task.get_docker_command()
+    assert cmd == 'docker run --rm -v /staging/test_wf/mult_3/tmp:/tmp -v /staging/test_wf/add_5/data/output.txt:/tmp/input jstubbs/mult_n python mult_n.py -f 3'
