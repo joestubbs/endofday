3e6fe1ec1a791677e241cd97156b2baa1faf6c2d
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-04-28 16:02:01 -0500

    Pass the verbose flag.

--- endofday/core/endofday.sh
+++ endofday/core/endofday.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ARG=$1
+VERB=$2
 
 if [ $ARG = "--setup" ]; then
   cp /core/alias.sh /staging/endofday.sh
   chmod +x /staging/endofday.sh
 else
   cd /
-  python -m core.tasks $STAGING/$ARG
+  python -m core.tasks $STAGING/$ARG $VERB
   # This used only for the nf backend
 #  /nextflow run "${ARG%.*}.nf"
 fi
