1af96586c151c55735a713702544a445207d4bf6
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-04-28 16:01:41 -0500

    Mount the host so global inputs are available in the eod container.

--- endofday/core/alias.sh
+++ endofday/core/alias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v $(pwd):/staging -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/endofday $*
+docker run -v /:/host -v $(pwd):/staging -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/endofday $*
