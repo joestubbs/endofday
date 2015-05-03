c3cbacd48981fdbe0463ae90115cc3672ca809c5
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-05-03 10:56:36 -0500

    Automatically read config from the cwd when running in docker.

--- endofday/core/config.py
+++ endofday/core/config.py
@@ -1,8 +1,9 @@
 """
-Read config files from:
-- Deployed endofday.conf inside package
-- /etc/endofday.conf
+Config files are read from the following places in order, with subsequent reads overwriting prior ones:
 - /endofday.conf
+- /etc/endofday.conf
+- Deployed endofday.conf inside package
+- When running in docker, an endofday.conf in the current working directory (mounted to /staging in the container).
 
 """
 
@@ -22,9 +23,11 @@ class AgaveConfigParser(ConfigParser.ConfigParser):
 
 def read_config():
     parser = AgaveConfigParser()
-    places = [os.path.abspath(os.path.join(HERE, '../endofday.conf')),
+    print "contents of staging dir:", str(os.listdir('/staging'))
+    places = ['/endofday.conf',
               os.path.expanduser('/etc/endofday.conf'),
-              '/endofday.conf']
+              os.path.abspath(os.path.join(HERE, '../endofday.conf')),
+              '/staging/endofday.conf',]
     if not parser.read(places):
         raise Error("couldn't read config file from {0}"
                            .format(', '.join(places)))
