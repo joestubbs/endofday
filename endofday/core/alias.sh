3a0f2cce83f2b8c7f6927a6cd69a8f06486e31b0
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-02-23 15:23:20 -0600

    Mount docker binary from host to prevent client/server version mismatch.

--- endofday/core/alias.sh
+++ endofday/core/alias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker run -t -v /:/host -v $(pwd):/staging -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/eod $*
+cmd=$(which docker)
+docker run -t -v /:/host -v $(pwd):/staging -v $cmd:/usr/bin/docker -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/eod $*
