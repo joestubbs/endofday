cf2e0c704d704f9976aaada45d4d6ff9ccd54894
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-02-23 15:25:58 -0600

    Update examples to include approx_pi_synch which uses directory inputs/dependencies.

--- examples/images/ctpts/ctpoints.py
+++ examples/images/ctpts/ctpoints.py
@@ -1,33 +1,40 @@
 from argparse import ArgumentParser
 import decimal
-import random
+import os
 
 def ctpoints(path):
     """
-    count coordinates in a cirle from an input file. 
+    count coordinates in a cirle from an input file or directory.
     """
     incirc = 0
     tot = 0
     out = open('/tmp/output', 'w')
-    with open (path, 'rb') as f:
-        for line in f:
- #           try:
+    # if they passed a directory, process all files at the top level
+    if os.path.isdir(path):
+        for _, _, fnames in os.walk(path):
+            files = [os.path.join(path, f) for f in fnames]
+            break
+    else:
+        files = [path]
+    print "Reading these files:"
+    for f in files:
+        print f
+    for f in files:
+        with open (f, 'rb') as f:
+            for line in f:
                 parts = line.split(',')
                 x = decimal.Decimal(parts[0].strip())
                 y = decimal.Decimal(parts[1].strip())
                 if x*x + y*y <= 1:
                     incirc += 1
                 tot += 1
-#            except Exception as e:
-#                print "Got an exception:", str(e)
-#                raise e
     out.write(str(incirc) + ',' + str(tot))
     out.close()
 
 def main():
     parser = ArgumentParser(description="count coordinates in a circle.")
     parser.add_argument('-v', '--version', action='version', version="0.1")
-    parser.add_argument('-p', '--path', help='path to input file to process')    
+    parser.add_argument('-p', '--path', help='path to input file or directory to process')
     args = parser.parse_args()
     ctpoints(args.path)
 
--- examples/images/genpoints/genpoints.py
+++ examples/images/genpoints/genpoints.py
@@ -27,12 +27,12 @@ def from_config(args):
     args.files = config.get('genpoints', 'files')
     return args
 
-def genpoints(n, f):
+def genpoints(n, f, path):
     """
-    Generates n coordinates into t output files labeled 
+    Generates n coordinates into t output files labeled out_$i in the path directory.
     """
     for i in range(f):
-        out = open('/data/out_' + str(i), 'w')
+        out = open('{}/out_{}'.format(path,i), 'w')
         for j in range(n):
             x = random.random()
             y = random.random()
@@ -45,6 +45,7 @@ def main():
     parser.add_argument('-n', '--coords', help='number of coords to generate per file') 
     parser.add_argument('-f', '--files', help='number of files to generate')
     parser.add_argument('-p', '--path', help='path to config file.')
+    parser.add_argument('-o', '--output', help='path to write outputs to.')
     args = parser.parse_args()
     if args.path:
         args = from_config(args)
@@ -53,7 +54,9 @@ def main():
         args.coords = 1000
     if not args.files:
         args.files = 4
-    genpoints(int(args.coords), int(args.files))
+    if not args.output:
+        args.output = '/output'
+    genpoints(int(args.coords), int(args.files), args.output)
 
 if __name__ == "__main__":
     main()
