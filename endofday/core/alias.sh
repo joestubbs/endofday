a833d3cc505a1f61713bb0888c0c7ed4565715e7
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-02-26 17:56:27 -0600

    Pass username as an env var

--- endofday/core/alias.sh
+++ endofday/core/alias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cmd=$(which docker)
-docker run -t -v /:/host -v $(pwd):/staging -v $cmd:/usr/bin/docker -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/eod $*
+docker run -t -v /:/host -v $(pwd):/staging -v $cmd:/usr/bin/docker -e AGAVE_USERNAME=$username -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/eod $*
