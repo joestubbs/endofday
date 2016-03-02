ae5602986ec4250e22f3388633054a5695768543
Joe Stubbs <joestubbs5@gmail.com>
2016-03-02 00:50:17 -0600

    Compute docker host server version to prevent client/server version mismatch errors.

--- endofday/core/alias.sh
+++ endofday/core/alias.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cmd=$(which docker)
-docker run -t -v /:/host -v $(pwd):/staging -v $cmd:/usr/bin/docker -e AGAVE_USERNAME=$username -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/eod $*
+version=$(docker version -f '{{.Server.APIVersion}}')
+docker run -t -v /:/host -v $(pwd):/staging -e DOCKER_API_VERSION=$version -e AGAVE_USERNAME=$username -e RUNNING_IN_DOCKER=true -e STAGING_DIR=$(pwd) --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/eod $*
